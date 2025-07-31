ef99b2cca27d2f6a33e01a42be62a708fc46dd44
rizwans-github <rizwankhan0964e@gmail.com>
2025-07-31 22:39:44 +0530

    refactor: automate running of Python script

---
+++ Automation.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Change directory to your script folder
+cd /d C:\Users\rizwa\Tech\Github\Git-automation
+
+REM Run Python script
+python main.py
+
+REM Keep window open for debugging (optional)
+pause
